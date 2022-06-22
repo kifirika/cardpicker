f2aff7025c9bb28843e8e2bb367113c80cb9fffe
Vladislav Nesterenko <vladnesterenk0@mail.ru>
2022-06-22 13:05:32 +0400

    Добавьте файлы проекта.

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PickRandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of cards to pick: ");
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter the valid number");
+            }
+        }
+    }
+}
